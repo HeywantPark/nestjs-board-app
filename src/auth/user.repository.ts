75d51b811574bdb91d289e0adbc36e98307632b0
Park Hyewon <db1648@naver.com>
2025-04-29 17:21:53 +0900

    "feat : 유저 이름에 유니크한 값 부여"

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,6 +2,10 @@ import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { CustomUserRepository } from './user-custom-repository.interface';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 export const UserRepository = (
   dataSource: DataSource,
@@ -15,7 +19,16 @@ export const UserRepository = (
       const { username, password } = authCredentialDto;
       const user = this.create({ username, password });
 
-      await this.save(user);
+      try {
+        await this.save(user);
+      } catch (error) {
+        const err = error as { code?: string };
+        if (err.code === '23505') {
+          throw new ConflictException('Existing username');
+        } else {
+          throw new InternalServerErrorException();
+        }
+      }
     },
   });
 };
