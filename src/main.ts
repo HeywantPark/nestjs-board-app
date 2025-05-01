45aa034426234acdc4a73c37a25e9ff1fb02f19e
Park Hyewon <db1648@naver.com>
2025-05-01 17:22:32 +0900

    "refactor : 설정 파일 사용"

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
+import * as config from 'config';
+
+interface ServerConfig {
+  port: number;
+  host: string;
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
+
+  const serverConfig = config.get<ServerConfig>('server');
+  const port = serverConfig.port;
+
   await app.listen(process.env.PORT ?? port);
   Logger.log(`Application running on port ${port}`);
 }
